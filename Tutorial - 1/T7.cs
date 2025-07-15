0f215b296791f81e2c1d142d35159282d1c72f76
Hemal Rashiya <hrasiya212@rku.ac.in>
2025-07-15 15:39:07 +0530

    new code added

--- Tutorial - 1/T7.cs
+++ Tutorial - 1/T7.cs
@@ -8,5 +8,24 @@ namespace Tutorial___1
 {
     internal class T7
     {
+        public static void t7()
+        {
+            int num = 123;
+            int ans = 0;
+            int temp = num;
+
+            while(temp != 0)
+            {
+                
+                num = temp % 10;
+                Console.WriteLine("Current digit: " + num);
+                ans = (ans * 10) + num;
+                Console.WriteLine("Current answer: " + ans);
+                temp = temp / 10;
+                Console.WriteLine("Remaining number: " + temp);
+            }
+            Console.WriteLine("The reverse of the number is: " + ans);
+
+        }
     }
 }
