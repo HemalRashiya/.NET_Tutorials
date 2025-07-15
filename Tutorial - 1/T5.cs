992645a3d12dd41375b123859354ed54e207c726
Hemal Rashiya <hrasiya212@rku.ac.in>
2025-07-15 15:00:03 +0530

    new code added

--- Tutorial - 1/T5.cs
+++ Tutorial - 1/T5.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,32 @@ namespace Tutorial___1
     {
         public static void t5()
         {
-            
+            int n, fact = 1; 
+            //Intialize the variable 'fact' to 1 and also intiaalize n.
+
+            Console.WriteLine("Enter Number : ");
+            //Print a statement 'enter number :' using writeline method.
+
+            string str = Console.ReadLine();
+            //Take a input from the user suing ReadLine method and store it in a string variable 'str'.
+            n = Convert.ToInt32(str);
+            //Convert str value into integer using Convert.ToInt32 method and stroe into n.
+
+
+            for (int i = 1; i <= n; i++)
+               //in for loop intialize i to 1 and check or run loop untill i is less than n.
+            {
+
+                fact = fact * i;
+                //Multiply the value of fact with i and store it in fact.
+
+            }
+
+            Console.WriteLine("Factorial : {0}", fact);
+            //Print a factorial value using WriteLine method.
+
+
+            Console.Read();
         }
     }
 }
